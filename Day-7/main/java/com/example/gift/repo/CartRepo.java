e91c43115a57cad7f9e47a950e42f5dca1d233d5
Aakashsakthi12 <120013331+Aakashsakthi12@users.noreply.github.com>
2024-03-27 19:22:10 +0530

    Delete Day-7/main directory

--- Day-7/main/java/com/example/gift/repo/CartRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.gift.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.gift.entity.CartEntity;
-
-public interface CartRepo extends JpaRepository<CartEntity,Integer>{
-
-}
